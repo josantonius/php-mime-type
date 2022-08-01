680446379e0e6a5fa3055ee79668664f0a28c087
Josantonius <hello@josantonius.dev>
2022-08-02 01:17:17 +0200

    feat: main upload of v2.0.0 version
    Closes #7

--- src/MimeType.php
+++ src/MimeType.php
@@ -1,53 +1,42 @@
 <?php
-/**
- * PHP library for obtain headers MIME.
- *
- * @author    Josantonius <hello@josantonius.com>
- * @copyright 2016 - 2018 (c) Josantonius - PHP-MimeType
- * @license   https://opensource.org/licenses/MIT - The MIT License (MIT)
- * @link      https://github.com/Josantonius/PHP-MimeTypeype
- * @since     1.0.0
- */
+
+/*
+* This file is part of https://github.com/josantonius/php-mime-type repository.
+*
+* (c) Josantonius <hello@josantonius.dev>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Josantonius\MimeType;
 
 /**
- * Get MIME type and file extensions.
+ * MIME type and file extension handler.
  */
 class MimeType
 {
     /**
      * Get array with all MIME types.
-     *
-     * @since 1.1.3
-     *
-     * @return array → MIME types
      */
-    public static function get()
+    public function getAll(): array
     {
-        return MimeTypesCollection::all();
-    }
-
-    /**
-     * Get MIME type from file extension.
-     *
-     * @param string $ext → file extension, e.g. '.html'
-     *
-     * @return string|false → MIME type or false
-     */
-    public static function getMimeFromExtension($ext)
-    {
-        return MimeTypesCollection::get($ext) ?: false;
+        return MimeTypeCollection::getAll();
     }
 
     /**
      * Get file extension from MIME type.
-     *
-     * @param string $mime → MIME type, e.g. 'text/html'
-     *
-     * @return string|false → file extension or false
      */
-    public static function getExtensionFromMime($mime)
+    public function getExtension(string $mimeType): ?string
     {
-        return array_search($mime, MimeTypesCollection::all(), true);
+        return MimeTypeCollection::getExtension($mimeType);
+    }
+
+    /**
+     * Get MIME type from file extension.
+     */
+    public function getMime(string $extension): ?string
+    {
+        return MimeTypeCollection::getMime($extension);
     }
 }
